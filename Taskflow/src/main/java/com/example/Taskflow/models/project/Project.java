4446e8f9b5bdc9cc414564c091802753ad4508d5
cristianpetrea <petreacristian38@yahoo.com>
2025-10-15 22:31:46 +0300

    Adnotation added for User and Project classes

--- Taskflow/src/main/java/com/example/Taskflow/models/project/Project.java
+++ Taskflow/src/main/java/com/example/Taskflow/models/project/Project.java
@@ -1,20 +1,28 @@
 package com.example.Taskflow.models.project;
 
 import com.example.Taskflow.models.user.User;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
-
+@Entity
+@Table(name="projects")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String name;
+    @Column(columnDefinition = "TEXT")
     private String description;
+    @Column(nullable = false)
     private Timestamp creation_date;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
--- Taskflow/src/main/java/com/example/Taskflow/models/user/User.java
+++ Taskflow/src/main/java/com/example/Taskflow/models/user/User.java
@@ -1,16 +1,36 @@
 package com.example.Taskflow.models.user;
 
+
+import com.example.Taskflow.models.project.Project;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private String role;
+    @Column(nullable = false)
     private Timestamp register_date;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Project> ownedProject=new HashSet<>();
+
 }
